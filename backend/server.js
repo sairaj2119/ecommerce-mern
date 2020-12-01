5ff4fe823fcb8b622a8abfe47963779fc3ae5d2c
sairaj <sairaj2119@gmail.com>
2020-12-01 22:49:56 +0530

    connected to database

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 import products from './data/products.js';
+import connectDB from './config/db.js';
 
 dotenv.config();
+connectDB();
+
 const app = express();
 
 app.use(cors());
@@ -21,5 +24,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(
     `server runnning in ${process.env.NODE_ENV.toUpperCase()} mode on port ${PORT}`
+      .black.bgWhite
   );
 });
