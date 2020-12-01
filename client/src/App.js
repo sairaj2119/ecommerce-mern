27de511a66905a853d869f62ab898389c414fce6
sairaj <sairaj2119@gmail.com>
2020-12-01 17:38:23 +0530

    added react-bootstrap Header and Footer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 function App() {
-  return <div>app</div>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>YOO man</Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
