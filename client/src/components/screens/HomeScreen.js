ba804415b3081a7470a33a5a7e970df85bb81546
sairaj <sairaj2119@gmail.com>
2020-12-03 12:31:54 +0530

    added a Message and a Spinner components

--- client/src/components/screens/HomeScreen.js
+++ client/src/components/screens/HomeScreen.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Product from '../Product';
 import { listProducts } from '../../actions/productActions';
+import Loader from '../Loader';
+import Message from '../Message';
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
@@ -18,9 +20,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest products</h1>
       {loading ? (
-        <h2>Loading..</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
