5b95a9e40c0e966295ffdceee4dbd394e005df91
sairaj <sairaj2119@gmail.com>
2020-12-03 21:31:01 +0530

    implemented middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 const router = Router();
 
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
